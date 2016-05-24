2284f5a8801efdf43208a17ceedf6f13f2064713
Will Gabriel <williamgabriel04@gmail.com>
2016-05-24 10:59:26 -0300

    redefinir senha view models

---
+++ Mazul/AgendaApp/Models/RedefinirSenhaViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AgendaApp.Models
+{
+    public class RedefinirSenhaViewModels
+    {
+        public int UsuarioId { get; set; }
+
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "As senhas devem ser iguais.")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
