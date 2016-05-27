d119ca0d6021bde63a4eb412fe6dec4e2024541f
Will <williamgabriel04@gmail.com>
2016-05-26 21:17:06 -0300

    texto tela inicial e ajuste nos alertas

--- Mazul/AgendaApp/Scripts/mazulUtils.js
+++ Mazul/AgendaApp/Scripts/mazulUtils.js
@@ -1,3 +1,3 @@
 ﻿function alerta(mensagem, tipo) {
-    Materialize.toast(mensagem, 70000, tipo == 'sucesso' ? 'green white-text' : 'red white-text');
+    Materialize.toast(mensagem, 7000, tipo == 'sucesso' ? 'green white-text' : 'red white-text');
 }
